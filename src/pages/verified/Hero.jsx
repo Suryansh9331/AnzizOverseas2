521379c1b35022b18d9265202fef820525e1f41f
suryansh9331 <suryanshmishra9331@gmail.com>
2025-09-01 05:06:09 +0530

    Sections improved

--- src/pages/verified/Hero.jsx
+++ src/pages/verified/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BgImg from "../../assets/verefied/bg2.png";
+import BgImg from "../../assets/verefied/bg3.png";
 import ReadMoreArrowImg from "../../assets/verefied/read-more-arrow.png";
 
 export default function Hero() {
@@ -10,18 +10,18 @@ export default function Hero() {
     >
       <div className="max-w-7xl w-full flex items-center justify-between">
         <div className="max-w-3xl">
-          <h1 className="text-6xl lg:text-7xl font-bold mb-6">
+          <h1 className="text-4xl md:text-7xl lg:text-7xl font-bold mb-6">
             <div className="text-white mb-3">Verified Exporter</div>
             <div className="text-[#1DA1F2]">Showroom</div>
           </h1>
 
-          <p className="text-white text-xl lg:text-xl mb-10 leading-relaxed max-w-xl">
+          <p className="text-white text-md mb-4  md:text-lg  lg:text-lg mb-10 leading-relaxed lg:max-w-lg md:max-w-md max-w-xs  ">
             Browse trusted, pre-verified exporters from around the globe — ready
             to ship quality products directly to you.
           </p>
 
           <button
-            className="bg-transparent border-2 border-white text-white px-8 py-4 rounded-full hover:bg-[#1DA1F2] hover:text-white transition-all duration-300 flex items-center gap-3 group"
+            className=" bg-transparent border-2 border-white text-white px-8 py-4 rounded-full hover:bg-[#1DA1F2] hover:text-white transition-all duration-300 flex items-center gap-3 group"
             onClick={() =>
               window.scrollBy({
                 top: window.innerHeight,
@@ -31,11 +31,21 @@ export default function Hero() {
             }
           >
             <span className="text-lg font-medium">READ MORE</span>
-            <img
-              src={ReadMoreArrowImg}
-              alt="arrow"
-              className="w-10 h-6 group-hover:translate-x-1 transition-transform duration-300"
-            />
+           <span className="w-[71px] h-[40px]">
+                <svg
+                  width="71"
+                  height="40"
+                  viewBox="0 0 71 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M55.7071 20.7071C56.0976 20.3166 56.0976 19.6834 55.7071 19.2929L49.3431 12.9289C48.9526 12.5384 48.3195 12.5384 47.9289 12.9289C47.5384 13.3195 47.5384 13.9526 47.9289 14.3431L53.5858 20L47.9289 25.6568C47.5384 26.0474 47.5384 26.6805 47.9289 27.0711C48.3195 27.4616 48.9526 27.4616 49.3431 27.0711L55.7071 20.7071ZM0 20L8.74228e-08 21L55 21L55 20L55 19L-8.74228e-08 19L0 20Z"
+                    fill="#ffffff"
+                  />
+                  <circle cx="51" cy="20" r="19.5" stroke="#1DA1F2" />
+                </svg>
+              </span>
           </button>
         </div>
 
